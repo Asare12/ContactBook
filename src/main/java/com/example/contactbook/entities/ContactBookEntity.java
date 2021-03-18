380a973370aa372bc971b6f69a332a765af4b922
davidasare <gohan.david11@gmail.com>
2021-03-18 17:06:12 +0000

    fix: correct minor typo in code & deleting contactbook class

--- src/main/java/com/example/contactbook/entities/ContactBookEntity.java
+++ src/main/java/com/example/contactbook/entities/ContactBookEntity.java
@@ -5,50 +5,47 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-public class ContactBookEntity {
+@Entity
+public class ContactBook {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer contactBookId;
 
-    @Entity
-    public class ContactBook {
-        @Id
-        @GeneratedValue(strategy= GenerationType.AUTO)
-        private Integer contactBookId;
+    private String name;
 
-        private String name;
+    private String email;
 
-        private String email;
+    private String phoneNumber;
 
-        private String phoneNumber;
+    public Integer getContactBookId() {
+        return contactBookId;
+    }
 
-        public Integer getContactBookId() {
-            return contactBookId;
-        }
+    public void setContactBookId(Integer contactBookId) {
+        this.contactBookId = contactBookId;
+    }
 
-        public void setContactBookId(Integer contactBookId) {
-            this.contactBookId = contactBookId;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public String getName() {
-            return name;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-        public void setName(String name) {
-            this.name = name;
-        }
+    public String getEmail() {
+        return email;
+    }
 
-        public String getEmail() {
-            return email;
-        }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-        public void setEmail(String email) {
-            this.email = email;
-        }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-        public String getPhoneNumber() {
-            return phoneNumber;
-        }
-
-        public void setPhoneNumber(String phoneNumber) {
-            this.phoneNumber = phoneNumber;
-        }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
--- src/test/java/com/example/contactbook/ContactBookEntityApplicationTests.java
+++ src/test/java/com/example/contactbook/ContactBookEntityApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.contactbook;
+package com.contactbook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
