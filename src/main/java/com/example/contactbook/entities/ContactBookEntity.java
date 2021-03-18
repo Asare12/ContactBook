31ca4138de46b703bbe572b70d5437ee506354a5
davidasare <gohan.david11@gmail.com>
2021-03-18 16:35:38 +0000

    feat: Added enitity class

---
+++ src/main/java/com/example/contactbook/entities/ContactBookEntity.java
@@ -0,0 +1,54 @@
+package com.example.contactbook.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class ContactBookEntity {
+
+    @Entity
+    public class ContactBook {
+        @Id
+        @GeneratedValue(strategy= GenerationType.AUTO)
+        private Integer contactBookId;
+
+        private String name;
+
+        private String email;
+
+        private String phoneNumber;
+
+        public Integer getContactBookId() {
+            return contactBookId;
+        }
+
+        public void setContactBookId(Integer contactBookId) {
+            this.contactBookId = contactBookId;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+    }
+}
