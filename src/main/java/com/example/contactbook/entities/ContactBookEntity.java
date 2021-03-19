c3ca2f5faf0097faf962db6f01840656b97ddb9a
davidasare <gohan.david11@gmail.com>
2021-03-19 11:37:49 +0000

    fix: changed entity class name

--- src/main/java/com/example/contactbook/entities/ContactBookEntity.java
+++ src/main/java/com/example/contactbook/entities/ContactBookEntity.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class ContactBook {
+public class ContactBookEntity {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer contactBookId;
