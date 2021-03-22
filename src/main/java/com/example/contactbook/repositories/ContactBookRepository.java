2a7cf7463fd0644c26f5655f688ad5420f694e38
davidasare <gohan.david11@gmail.com>
2021-03-22 19:28:15 +0000

    feat: Added a new method to the interface and added docblock

--- src/main/java/com/example/contactbook/repositories/ContactBookRepository.java
+++ src/main/java/com/example/contactbook/repositories/ContactBookRepository.java
@@ -2,10 +2,21 @@ package com.example.contactbook.repositories;
 
 import com.example.contactbook.entities.ContactBookEntity;
 import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
 public interface ContactBookRepository extends CrudRepository<ContactBookEntity, Integer> {
 
+    /**
+     * Find a contact with given name
+     * @param name
+     * @return the contact with matching name
+     */
     ContactBookEntity findByName(String name);
 
-    void deleteByName(String name);
+    /**
+     * Find all contacts with a specific name
+     * @param name
+     * @return the list contacts with matching name
+     */
+    List<ContactBookEntity> findByNameContaining(String name);
 }
