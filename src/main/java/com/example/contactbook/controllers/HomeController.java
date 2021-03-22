e4cb329bd9602f2a8c38786f4f6e3b90afba0a78
davidasare <gohan.david11@gmail.com>
2021-03-22 16:56:32 +0000

    feat: Creating root endpoint

--- src/main/java/com/example/contactbook/controllers/HomeController.java
+++ src/main/java/com/example/contactbook/controllers/HomeController.java
@@ -1,2 +1,17 @@
-package com.example.contactbook.controllers;public class HomeController {
+package com.example.contactbook.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    /**
+     * Handles the root (/) endpoint
+     * @return index page
+     */
+    @RequestMapping("/")
+    private String viewIndexPage(){
+        return "index";
+    }
 }
