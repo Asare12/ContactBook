47c2e801e1a4b8ffc630a01e4684617c546a347d
davidasare <gohan.david11@gmail.com>
2021-03-22 16:58:33 +0000

    refactor: Adding lombok to entity

--- src/main/java/com/example/contactbook/entities/ContactBookEntity.java
+++ src/main/java/com/example/contactbook/entities/ContactBookEntity.java
@@ -1,51 +1,18 @@
 package com.example.contactbook.entities;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class ContactBookEntity {
+public @Data class ContactBookEntity {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer contactBookId;
-
     private String name;
-
     private String email;
-
     private String phoneNumber;
-
-    public Integer getContactBookId() {
-        return contactBookId;
-    }
-
-    public void setContactBookId(Integer contactBookId) {
-        this.contactBookId = contactBookId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
 }
